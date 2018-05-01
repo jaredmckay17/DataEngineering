afff101d82cbb745dbdc24d08c2cd5006d092924
Jared Christensen <36650115+jaredmckay17@users.noreply.github.com>
2018-04-30 19:27:31 -0600

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -4,25 +4,25 @@ import dag
 
 class Pipeline: 
     def __init__ (self):
-	    self.tasks = dag.DAG()
+        self.tasks = dag.DAG()
 
     def task(self, depends_on = None):
-	    def inner(f):
-		    self.tasks.add(f)
-			if depends_on: 
-				self.tasks.add(depends_on, f)
-			return f
-		return inner 
+        def inner(f):
+            self.tasks.add(f)
+	        if depends_on: 
+	            self.tasks.add(depends_on, f)
+                    return f
+        return inner 
 
-	def run(self):
-		visited = self.tasks.sort()
-		completed = {}
+   def run(self):
+        visited = self.tasks.sort()
+        completed = {}
 
-		for task in visited:
-			for node, ptr in self.tasks.graph.items():
-				if task in ptr:
-					completed[task] = task(completed[node])
-			if task not in completed:
-				completed[task] = task()
+        for task in visited:
+            for node, ptr in self.tasks.graph.items():
+                if task in ptr:
+	            completed[task] = task(completed[node])
+            if task not in completed:
+                completed[task] = task()
+        return completed
 
-		return completed
